6570404e6b013d99804a2d64332b3a910ff513f1
kousuke <kousukeee0303@gmail.com>
2019-10-30 09:04:02 +0000

    fin-edit-user

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 100 }, uniqueness: true,
                     format: { with: VALID_EMAIL_REGEX }
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   
   def User.digest(string)
     cost = 
