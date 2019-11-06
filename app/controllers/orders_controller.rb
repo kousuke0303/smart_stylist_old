34cc9cde3936db1ab7ab656727e4f8faa594d44a
kousuke <kousukeee0303@gmail.com>
2019-11-06 06:55:36 +0000

    fin-create-order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,14 +2,21 @@ class OrdersController < ApplicationController
   before_action :set_user_by_user_id
   before_action :logged_in_user
   before_action :correct_user_by_user_id
+  before_action :set_clients_of_user, only: [:new, :create, :edit, :update]
+  before_action :set_plants_of_user, only: [:new, :create, :edit, :update]
   
   def new
     @order = Order.new
-    @clients = @user.clients.all
-    @plants = Plant.where(user_id: @user.id) 
   end
   
   def create
+    @order = Order.new(order_params)
+    if @order.save
+      flash[:success] = "新規オーダーを登録しました。"
+      redirect_to root_url
+    else
+      render :new
+    end
   end
   
   def index
@@ -26,4 +33,21 @@ class OrdersController < ApplicationController
   
   def destroy
   end
+  
+  # beforeフィルター
+  
+  def set_clients_of_user
+    @clients = @user.clients.all
+  end
+  
+  def set_plants_of_user
+    @plants = Plant.where(user_id: @user.id)
+  end
+  
+  private
+
+    def order_params
+      params.require(:order).permit(:client_id, :kind, :plant_id, :retail, :note, :order_date, :sales_date,
+                                    :delivery, :wage, :cloth, :lining, :button, :postage, :other)
+    end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -3,12 +3,12 @@ class Order < ApplicationRecord
   
   validates :kind, presence: true
   validates :order_date, presence: true
-  validates :retail, numericality: { only_integer: true, greater_than: 0, less_than: 100_000_000 }
-  validates :wage, numericality: { only_integer: true, greater_than: 0, less_than: 100_000_000 }
-  validates :cloth, numericality: { only_integer: true, greater_than: 0, less_than: 100_000_000 }
-  validates :lining, numericality: { only_integer: true, greater_than: 0, less_than: 100_000_000 }
-  validates :button, numericality: { only_integer: true, greater_than: 0, less_than: 100_000_000 }
-  validates :postage, numericality: { only_integer: true, greater_than: 0, less_than: 100_000_000 }
-  validates :other, numericality: { only_integer: true, greater_than: 0, less_than: 100_000_000 }
+  validates :retail, numericality: { only_integer: true, greater_than: 0, less_than: 100_000_000 }, allow_blank: true
+  validates :wage, numericality: { only_integer: true, greater_than: 0, less_than: 100_000_000 }, allow_blank: true
+  validates :cloth, numericality: { only_integer: true, greater_than: 0, less_than: 100_000_000 }, allow_blank: true
+  validates :lining, numericality: { only_integer: true, greater_than: 0, less_than: 100_000_000 }, allow_blank: true
+  validates :button, numericality: { only_integer: true, greater_than: 0, less_than: 100_000_000 }, allow_blank: true
+  validates :postage, numericality: { only_integer: true, greater_than: 0, less_than: 100_000_000 }, allow_blank: true
+  validates :other, numericality: { only_integer: true, greater_than: 0, less_than: 100_000_000 }, allow_blank: true
   validates :note, length: { maximum: 150 }
 end
