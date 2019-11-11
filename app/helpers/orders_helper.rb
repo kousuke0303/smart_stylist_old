21b2961f89b549665d6182b71a1d638c00f13864
kousuke <kousukeee0303@gmail.com>
2019-11-11 15:25:59 +0000

    fin-paid

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -44,4 +44,15 @@ module OrdersHelper
       order.wage_pay == true ? "支払済" : "未払い"
     end
   end
+  
+  def total_unpaid(order)
+    unpaid_w = order.wage if order.wage.present? && order.wage_pay == false
+    unpaid_c = order.cloth if order.cloth.present? && order.cloth_pay == false
+    unpaid_l = order.wage if order.lining.present? && order.lining_pay == false
+    unpaid_b = order.button if order.button.present? && order.button_pay == false
+    unpaid_p = order.postage if order.postage.present? && order.postage_pay == false
+    unpaid_o = order.other if order.other.present? && order.other_pay == false
+    @total_unpaid = unpaid_w.to_i + unpaid_c.to_i + unpaid_l.to_i +
+                   unpaid_b.to_i + unpaid_p.to_i + unpaid_o.to_i
+  end
 end
