88d483f252e025d4e2ba53df6f262ff41cb3fa9d
kousuke <kousukeee0303@gmail.com>
2019-11-13 08:17:10 +0000

    fin-plantform

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  require 'net/http'
+  require 'uri'
+  require 'json'
   include SessionsHelper
   include OrdersHelper
   
