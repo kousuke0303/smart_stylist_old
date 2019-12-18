8baeb3cf45965a0f37527a854d70c7cee99b40c4
kousuke <kousukeee0303@gmail.com>
2019-12-18 01:38:08 +0000

    fin-seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,28 +3,3 @@ User.create!(name: "Admin User",
              password: "password",
              password_confirmation: "password",
              admin: true)
-
-Plant.create!(name: "test_plant1",
-              tel_1: "0725654478",
-              user_id: 1)
-              
-Plant.create!(name: "test_plant2",
-              tel_1: "0725654447",
-              user_id: 1)
-
-60.times do |n|
-  name  = Faker::Name.name
-  email = "sample-#{n+1}@email.com"
-  password = "password"
-  User.create!(name: name,
-               email: email,
-               password: password,
-               password_confirmation: password)
-end
-
-60.times do |n|
-  name  = Faker::Name.name
-  user_id = "1"
-  Client.create!(name: name,
-                 user_id: user_id)
-end
