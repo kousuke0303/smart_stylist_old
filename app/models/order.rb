552dea721561008b4a1b1859ec95681c27e7f559
kousuke <kousukeee0303@gmail.com>
2020-01-31 04:26:51 +0000

    tp修正

--- app/models/order.rb
+++ app/models/order.rb
@@ -15,6 +15,7 @@ class Order < ApplicationRecord
   before_save { total_unpaid(self) > 0 ? self.unpaid = true : self.unpaid = false }
   before_save { self.retail = self.retail.to_i }
   before_save { self.traded = true if self.retail.to_i == self.deposit.to_i && self.sales_date.present? }
+  before_save { self.traded = false unless self.retail.to_i == self.deposit.to_i && self.sales_date.present?}
   
   validates :client_id, presence: true
   validates :kind, presence: true
