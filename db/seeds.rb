d3833f13ad5dda0149a56840bf7bad16d60b51a9
kousuke <kousukeee0303@gmail.com>
2020-03-18 09:24:33 +0000

    料金説明実装

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@ User.create!(name: "Admin User",
              question: "好きな食べ物は？",
              answer: "リンゴ",
              admin: true)
+
+User.create!(name: "test_user1",
+             email: "test1@email.com",
+             password: "password",
+             password_confirmation: "password",
+             question: "好きな食べ物は？",
+             answer: "リンゴ",)
+             
+User.create!(name: "test_user2",
+             email: "test2@email.com",
+             password: "password",
+             password_confirmation: "password",
+             question: "好きな食べ物は？",
+             answer: "リンゴ",)
