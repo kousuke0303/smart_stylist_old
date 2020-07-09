4b448ec2a970b5e51485004fb2bffdd1108f676c
kousuke <kousukeee0303@gmail.com>
2020-07-09 06:54:13 +0000

    fin_befsave

--- app/models/client.rb
+++ app/models/client.rb
@@ -3,14 +3,14 @@ class Client < ApplicationRecord
   has_many :orders, dependent: :destroy
   
   attr_accessor :zipcode
-  before_save { self.address = nil if address.match(ONLY_SPACE_REGEX) }
-  before_save { self.tel_1 = nil if tel_1.match(ONLY_SPACE_REGEX) }
-  before_save { self.tel_2 = nil if tel_2.match(ONLY_SPACE_REGEX) }
-  before_save { self.fax = nil if fax.match(ONLY_SPACE_REGEX) }
+  before_save { self.address = nil if !address.nil? && address.match(ONLY_SPACE_REGEX) }
+  before_save { self.tel_1 = nil if !tel_1.nil? && tel_1.match(ONLY_SPACE_REGEX) }
+  before_save { self.tel_2 = nil if !tel_2.nil? && tel_2.match(ONLY_SPACE_REGEX) }
+  before_save { self.fax = nil if !fax.nil? && fax.match(ONLY_SPACE_REGEX) }
   before_save { self.email = email.downcase unless email.nil? }
-  before_save { self.email = nil if email.match(ONLY_SPACE_REGEX) }
-  before_save { self.work = nil if work.match(ONLY_SPACE_REGEX) }
-  before_save { self.note = nil if note.match(ONLY_SPACE_REGEX) }
+  before_save { self.email = nil if !email.nil? && email.match(ONLY_SPACE_REGEX) }
+  before_save { self.work = nil if !work.nil? && work.match(ONLY_SPACE_REGEX) }
+  before_save { self.note = nil if !note.nil? && note.match(ONLY_SPACE_REGEX) }
 
   
   validates :name, presence: true, length: { maximum: 30 }
--- app/models/plant.rb
+++ app/models/plant.rb
@@ -2,15 +2,15 @@ class Plant < ApplicationRecord
   belongs_to :user
   
   attr_accessor :zipcode
-  before_save { self.address = nil if address.match(ONLY_SPACE_REGEX) }
-  before_save { self.tel_1 = nil if tel_1.match(ONLY_SPACE_REGEX) }
-  before_save { self.tel_2 = nil if tel_2.match(ONLY_SPACE_REGEX) }
-  before_save { self.fax = nil if fax.match(ONLY_SPACE_REGEX) }
+  before_save { self.address = nil if !address.nil? && address.match(ONLY_SPACE_REGEX) }
+  before_save { self.tel_1 = nil if !tel_1.nil? && tel_1.match(ONLY_SPACE_REGEX) }
+  before_save { self.tel_2 = nil if !tel_2.nil? && tel_2.match(ONLY_SPACE_REGEX) }
+  before_save { self.fax = nil if !fax.nil? && fax.match(ONLY_SPACE_REGEX) }
   before_save { self.email = email.downcase unless email.nil? }
-  before_save { self.email = nil if email.match(ONLY_SPACE_REGEX) }
-  before_save { self.staff_1 = nil if staff_1.match(ONLY_SPACE_REGEX) }
-  before_save { self.staff_2 = nil if staff_2.match(ONLY_SPACE_REGEX) }
-  before_save { self.note = nil if note.match(ONLY_SPACE_REGEX) }
+  before_save { self.email = nil if !email.nil? && email.match(ONLY_SPACE_REGEX) }
+  before_save { self.staff_1 = nil if !staff_1.nil? && staff_1.match(ONLY_SPACE_REGEX) }
+  before_save { self.staff_2 = nil if !staff_2.nil? && staff_2.match(ONLY_SPACE_REGEX) }
+  before_save { self.note = nil if !note.nil? && note.match(ONLY_SPACE_REGEX) }
   
   validates :name, presence: true, length: { maximum: 30 }
   validates :address, length: { maximum: 50 }
